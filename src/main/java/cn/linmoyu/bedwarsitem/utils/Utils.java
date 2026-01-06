85f97884046a5e4ea049ab1d887580c9b1653377
LinMoyuu <161136026+LinMoyuu@users.noreply.github.com>
2026-01-06 20:13:31 +0800

    fix:

--- src/main/java/cn/linmoyu/bedwarsitem/utils/Utils.java
+++ src/main/java/cn/linmoyu/bedwarsitem/utils/Utils.java
@@ -1,16 +1,14 @@
 package cn.linmoyu.bedwarsitem.utils;
 
 import io.github.bedwarsrel.game.Game;
-import io.github.bedwarsrel.game.ResourceSpawner;
 import io.github.bedwarsrel.game.Team;
+import me.ram.bedwarsscoreboardaddon.utils.BedwarsUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-
-import java.util.List;
 
 public class Utils {
 
@@ -20,12 +18,10 @@ public class Utils {
             return false;
         }
         for (Entity entity : location.getWorld().getNearbyEntities(location.clone().add(0.5, 1, 0.5), 0.5, 1, 0.5)) {
-            if (!(entity instanceof Player)) {
-                return false;
-            }
-            Player player = (Player) entity;
-            if (!game.isInGame(player) || game.isSpectator(player)) {
-                return false;
+            if (entity instanceof Player) {
+                if (!BedwarsUtil.isSpectator(game, ((Player) entity).getPlayer())) {
+                    return false;
+                }
             }
         }
         if (Bukkit.getPluginManager().isPluginEnabled("BedwarsScoreBoardAddon")) {
@@ -33,22 +29,6 @@ public class Utils {
                 for (Team team : game.getTeams().values()) {
                     if (team.getSpawnLocation().distanceSquared(block.getLocation().clone().add(0.5, 0, 0.5)) <= Math.pow(me.ram.bedwarsscoreboardaddon.config.Config.spawn_no_build_spawn_range, 2)) {
                         return false;
-                    }
-                }
-            }
-            if (me.ram.bedwarsscoreboardaddon.config.Config.spawn_no_build_resource_enabled) {
-                for (ResourceSpawner spawner : game.getResourceSpawners()) {
-                    if (spawner.getLocation().distanceSquared(block.getLocation().clone().add(0.5, 0, 0.5)) <= Math.pow(me.ram.bedwarsscoreboardaddon.config.Config.spawn_no_build_resource_range, 2)) {
-                        return false;
-                    }
-                }
-                if (me.ram.bedwarsscoreboardaddon.config.Config.game_team_spawner.containsKey(game.getName())) {
-                    for (List<Location> locs : me.ram.bedwarsscoreboardaddon.config.Config.game_team_spawner.get(game.getName()).values()) {
-                        for (Location loc : locs) {
-                            if (loc.distanceSquared(block.getLocation().clone().add(0.5, 0, 0.5)) <= Math.pow(me.ram.bedwarsscoreboardaddon.config.Config.spawn_no_build_resource_range, 2)) {
-                                return false;
-                            }
-                        }
                     }
                 }
             }
