7acda461fb2034f42242764f7d5585f98cac3b6a
LinMoyuu <161136026+LinMoyuu@users.noreply.github.com>
2026-02-12 16:03:24 +0800

    fix: 指南书 半角括号 缺失居中 换行

--- src/main/java/cn/linmoyu/bedwarsitem/items/TutorialBook.java
+++ src/main/java/cn/linmoyu/bedwarsitem/items/TutorialBook.java
@@ -30,14 +30,14 @@ public class TutorialBook implements Listener {
         BookMeta meta = (BookMeta) book.getItemMeta();
 
         // 从配置文件读取
-        String title = "§a§l起床战争指南§7（右键阅读）";
+        String title = "§a§l起床战争指南§7(右键阅读)";
         String author = ("一般路过羽末末");
 
         meta.setTitle(title);
         meta.setAuthor(author);
 
         // 第一页
-        String page1 = "§8§l      ● 起床战争指南\n\n" +
+        String page1 = "§8§l     ● 起床战争指南\n\n" +
                 "§8§l[♦] 基本玩法\n\n" +
                 "§8- 收集铁锭、金锭与钻石，然\n后在村民商店购买多种多样的道具！\n\n" +
                 "§8- 保护好你的床，被破坏后将\n永远无法复活，直至失败。\n\n" +
@@ -59,7 +59,7 @@ public class TutorialBook implements Listener {
                 " §8§l■ §r§8包含多种新玩法与体验";
 
         // 第四页
-        String page4 = "§8§l     ● 关于我们\n" +
+        String page4 = "§8§l     ● 关于我们\n\n" +
                 "§8《花雨庭》是由一群热爱我的世界的小伙伴们创建的国风小游戏服务器；上线伊始，我们与网易CC直播达成合作，相信在大家的支持下，我们会越来越好! 《花雨庭》只为你的\n快乐而生。\n\n" +
                 "§8§l ➜ R.I.P 2017-2025\n" +
                 "§r§8本端或原形已在哔哩哔哩免费发布.\n" +
